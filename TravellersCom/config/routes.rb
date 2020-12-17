1c23460084456e56d083f3f704ff13e97bae0d3e
Gayanika <nilosh@github.com>
2020-12-18 00:39:08 +0530

    Adding html and css stylings for the landing page.

--- TravellersCom/config/routes.rb
+++ TravellersCom/config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", 
     sign_up: "register" }
 
-  root to: 'pages#landing'
+  # root to: 'pages#landing'
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
   get "admin/dashboard" => "admin/dashboard#index"
   get "users/home" => "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
