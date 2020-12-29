ef7d5a599523bc33d1aa96a18af699688002a4a6
Gayanika <nilosh@github.com>
2020-12-29 12:56:09 +0530

    Edit remove button for user images.

--- TravellersCom/app/controllers/pages_controller.rb
+++ TravellersCom/app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   def landing
   end
 
+  def delete_image_attachment
+    @user.images.find_by(params[:attachment_id]).purge
+  end
+
+
 end
--- TravellersCom/db/schema.rb
+++ TravellersCom/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_083246) do
+ActiveRecord::Schema.define(version: 2020_12_29_055650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2020_12_22_083246) do
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
-
+  
   create_table "roles", force: :cascade do |t|
     t.string "role_name"
     t.datetime "created_at", precision: 6, null: false
