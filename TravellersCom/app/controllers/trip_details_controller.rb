78510f32a096bcd4ad2be3bb27c783cdf9ccc63c
Gayanika <nilosh@github.com>
2021-01-19 10:51:00 +0530

    Error fixed in search filter showing results

--- TravellersCom/app/controllers/trip_details_controller.rb
+++ TravellersCom/app/controllers/trip_details_controller.rb
@@ -22,11 +22,10 @@ class TripDetailsController < InheritedResources::Base
   # Queries for search and filters.
   def search
     if !params[:search].blank?
-      puts "Search from top nav bar."
       @trip_detail = TripDetail.search(params[:search])
     end
     if !params[:keywords].blank?
-      @trip_detail = TripDetail.where(["trip_details.location ILIKE ?", params[:keywords]])
+      @trip_detail = TripDetail.search(params[:keywords])
       @trip_detail = @trip_detail.where(["trip_details.start = ?", params[:start_date]]) if params[:start_date].present?
       @trip_detail = @trip_detail.where(["trip_details.end = ?", params[:end_date]]) if params[:end_date].present?
       if params[:gender].present?
